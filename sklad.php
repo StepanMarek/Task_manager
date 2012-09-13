825f5ff8b28e6d7fdff189b7525792149c2c96bc
Jirka Balhar <balhar.j@gmail.com>
2012-09-13 19:55:57 +0200

    Ikonky a filtrování podle tagů

--- sklad.php
+++ sklad.php
@@ -27,18 +27,24 @@ $pole_zaloh = getDirArray("sklad/105110102111", false);
 		bg = new GOLBackground( document.body, 256, 256, 6, 2);
 		bg.update(20);
 		bg.render();
-
-		// rozbalovací files
-		$(".file").on("click",function(){
-			var moreinfo = $(this).children(".moreinfo")
-			if( moreinfo.css("display") == "none" ){
-				moreinfo.css("display", "block");
+		$(".filtering .tags").on("keyup",function(){
+			if($(this).attr("value") == ""){
+				$(".file").css("display","block")
+				return;
 			}
-			else {
-				moreinfo.css("display", "none");
-			}
+			var search = $(this).attr("value").split(" ");
+			
+			$(".file").each(function(){
+				$(this).css("display","none");
+				var tags = $(this).data("tags").split(" ");
+				for(var i in search){
+					if($.inArray(search[i], tags) >= 0){
+						$(this).css("display","block");
+					}
+				}
+			})
 		})
-	})
+	});
 	</script>
 	<title>Skladiště</title>
 </head>
@@ -46,10 +52,10 @@ $pole_zaloh = getDirArray("sklad/105110102111", false);
 	<?php
 	include("header.php");
 	?>
-	<form>
-		Filtrovat podle tagů: <input type="text" placeholder="modely, dolni-patra, budova">
-		<input type="submit" value="Filtrovat">
-	</form>
+	<div class="filtering">
+		Filtrovat podle tagů: <input class="tags" type="text" placeholder="modely, dolni-patra, budova"><br>
+		Filtrovat podle jména: <input type="text" placeholder="Jan/Hynek/Štěpán/Jirka"><br>
+	</div>
 	<?php 
 	if($pole_zaloh){
 		for($i=0;$i<count($pole_zaloh);$i++){
@@ -57,10 +63,11 @@ $pole_zaloh = getDirArray("sklad/105110102111", false);
 				$linktask = getTask($pole_zaloh[$i]["link"], "tasks.txt")["name"];
 			else $linktask = "Příloha není vázána";
 			// přidat src a tags
-			echo "<div class='file' data-src='". $pole_zaloh[$i]["download"] ."' data-tags=''>
+			echo "<div class='file' data-src='". $pole_zaloh[$i]["download"] ."' data-tags='".$pole_zaloh[$i]["tags"]."'>
+				<img class='icon' src='http://www.stdicon.com/".$pole_zaloh[$i]["download"]."?size=92&default=http://www.stdicon.com/application/octet-stream'>
 				<span class='name'>".$pole_zaloh[$i]["name"]."</span>
-				<span class='tags'>Tagy: ".implode(", ", explode(" ", $pole_zaloh[$i]["tags"]))."</span>
-				<span class='date'>Přidáno ".date("d.m.Y G:i",$pole_zaloh[$i]["date"])."</span>
+				<span class='tags'><span class='important'>Tagy:</span> ".implode(", ", explode(" ", $pole_zaloh[$i]["tags"]))."</span><br>
+				<span class='date'><span class='important'>Přidáno</span> ".date("d.m.Y G:i",$pole_zaloh[$i]["date"])."</span>
 				<span class='download'>";
 		
 			if(substr($pole_zaloh[$i]["download"],0,7) != "http://"){
@@ -75,9 +82,9 @@ $pole_zaloh = getDirArray("sklad/105110102111", false);
 			echo "</span>";
 			echo	"
 				<div class='moreinfo'>
-					<span>Důležitost: ".$pole_zaloh[$i]["importancy"]."</span>
-					<span>Úkol: ".$linktask."</span>
-					<span class='type'>".$pole_zaloh[$i]["type"]."</span>
+					<br>
+					<span><span class='important'>Důležitost:</span> ".$pole_zaloh[$i]["importancy"]."</span>
+					<span><span class='important'>Úkol:</span> ".$linktask."</span><br>
 					<span class='description'>".$pole_zaloh[$i]["description"]."</span>
 				</div>
 				</div>";
